ca53d8de0b774b42ae67ad3cc01c308d4d3bed3b
Leonardo Celente <leocelente@gmail.com>
2022-04-05 12:21:46 -0300

    Formatting

--- windsim.py
+++ windsim.py
@@ -6,10 +6,9 @@ import argparse
 import datetime
 
 
-
 def main(args):
     # lat = #-22.0295#-11.4
-    # lon = #-47.8793#-41.28 
+    # lon = #-47.8793#-41.28
     # lat, lon = 37.428229, -122.168858 # stanford
 
     lat, lon = args.coordinate
@@ -17,10 +16,25 @@ def main(args):
     ut = args.time.hour*60
     year = args.time.year
 
-    print(f"Running HWM14 Model of Atmospheric Horizontal Wind Speed\n\tLocation: {lat}, {lon}\n\tTime: {args.time}\n\tAltitudes: {args.altitudes}")
+    day = time.timetuple().tm_yday
+    ut = time.hour * 60
+    year = time.year
 
     wind = HWM14(altlim=args.altitudes, altstp=1e-2, glat=lat, glon=lon, day=day, option=1, ut=ut, verbose=False, year=year)
         
+
+    wind = HWM14(
+        altlim=args.altitudes,
+        altstp=1e-2,
+        glat=lat,
+        glon=lon,
+        day=day,
+        option=1,
+        ut=ut,
+        verbose=False,
+        year=year,
+    )
+
     plt.figure(figsize=(5, 8))
     plt.plot(wind.Uwind, wind.altbins, wind.Vwind, wind.altbins)
     plt.title(f"Atmospheric Wind Model by Altitude\nat {lat}, {lon} @ {args.time}")
@@ -32,19 +46,37 @@ def main(args):
     plt.show()
 
 
-
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Graph Wind speed by altitude at location',prog="wind")
-    parser.add_argument('--coordinate', metavar=("LAT", "LON"), type=float, nargs=2,
-                        help='decimal coordinates latitude and longitude ', default=[-22.029, -47.8793])
-    
-    parser.add_argument('--altitudes', metavar=("START", "END"), type=float, nargs=2,
-                        help='range of altitudes in km', default=[.850, 30.0])
-    
-    parser.add_argument('--time', metavar='DATETIME', type=datetime.datetime.fromisoformat, nargs=1,
-                        help='ISO format date-time', default=datetime.datetime(2021, 10, 17, hour=8))
-    
+    parser = argparse.ArgumentParser(
+        description="Graph Wind speed by altitude at location", prog="wind"
+    )
+    parser.add_argument(
+        "--coordinate",
+        metavar=("LAT", "LON"),
+        type=float,
+        nargs=2,
+        help="decimal coordinates latitude and longitude ",
+        default=[-22.029, -47.8793],
+    )
+
+    parser.add_argument(
+        "--altitudes",
+        metavar=("START", "END"),
+        type=float,
+        nargs=2,
+        help="range of altitudes in km",
+        default=[0.850, 30.0],
+    )
+
+    parser.add_argument(
+        "--time",
+        metavar="DATETIME",
+        type=datetime.datetime.fromisoformat,
+        nargs=1,
+        help="ISO format date-time",
+        default=datetime.datetime(2021, 10, 17, hour=8),
+    )
+
     args = parser.parse_args()
 
     main(args)
